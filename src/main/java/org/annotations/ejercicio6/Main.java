9728af627e14bd0b43f0887d75f739f41fc90cfe
ivanluque-daw <iluqlop0905@g.educaand.es>
2024-10-07 04:16:19 +0200

    prepare ex 6

---
+++ src/main/java/org/annotations/ejercicio6/Main.java
@@ -0,0 +1,10 @@
+package org.annotations.ejercicio6;
+
+import org.annotations.ejercicio6.models.PersonaAutoData;
+
+public class Main {
+    public static void main(String[] args) {
+        PersonaAutoData persona = new PersonaAutoData();
+        System.out.println(persona);
+    }
+}
---
+++ src/main/java/org/annotations/ejercicio6/models/Persona.java
@@ -0,0 +1,9 @@
+package org.annotations.ejercicio6.models;
+
+import org.autodata.AutoData;
+
+@AutoData
+public class Persona {
+    private String nombre;
+    private int edad;
+}
